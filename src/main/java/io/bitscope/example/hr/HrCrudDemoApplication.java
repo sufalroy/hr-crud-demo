7f65fcb4e0f690ad750b87bd729f8d1cd3c541a0
sufal <sufalroy1997@gmail.com>
2022-03-06 09:43:20 +0530

    Initial Spring Project Setup

---
+++ src/main/java/io/bitscope/example/hr/HrCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package io.bitscope.example.hr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCrudDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bitscope/example/hr/HrCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bitscope.example.hr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
